66468a9d4c8a97f5433ac933607ae92c0fcaca31
Kudryashov <Jahma123@icloud.com>
2018-11-28 13:20:26 +0300

    installing nginx config

--- provision/always-on-root.sh
+++ provision/always-on-root.sh
@@ -1,3 +1,1 @@
 #!/bin/bash
-
-sudo systemctl start nginx
--- provision/once-as-root.sh
+++ provision/once-as-root.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-yum update
-yum install -y nginx
+yum update -y
 yum install -y epel-release yum-utils
+yum install -y nginx
 yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum-config-manager --enable remi-php71
 yum install -y php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysqlsudo cp sync/project.local /etc/nginx/sites-available/project.local
-cd /www/vhosts/sync/config
-cp project.local.conf /etc/nginx/conf.d/project.local.conf
-cd /etc/nginx/conf.d
+cp /www/vhosts/sync/sync/project.local.conf /etc/nginx/conf.d/project.local.conf
 yum install -y php-fpm
 systemctl enable nginx
