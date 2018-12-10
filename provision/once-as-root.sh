a44a4fb9d9cc43a1f017c562512f493187e697e6
Kudryashov <jahma123@icloud.com>
2018-12-10 19:41:36 +0300

    nginx+php+mysql (not tested)

--- provision/once-as-root.sh
+++ provision/once-as-root.sh
@@ -13,5 +13,13 @@ sudo chcon -R -t httpd_sys_content_t /www/vhosts/project.local
 sudo mv /etc/php-fpm.d/www.conf /etc/php-fpm.d/www.conf.backup
 sudo cp /home/vagrant/project.local/www.conf /etc/php-fpm.d/www.conf
 sudo chmod 777 /etc/php-fpm.d/www.conf
+sudo mv /etc/hosts /etc/hosts.backup
+sudo cp /home/vagrant/project.local/hosts.txt /etc/hosts
 sudo systemctl start nginx
-
+sudo wget http://repo.mysql.com/mysql57-community-release-el7.rpm
+sudo rpm -ivh mysql57-community-release-el7.rpm
+sudo yum update
+sudo yum install mysql-server
+sudo systemctl start mysqld
+sudo mysql_secure_installation
+sduo grep 'temporary password' /var/log/mysqld.log
