57dd2866a9a096d798f9bed0fa9ffa22562b012b
Kudryashov <jahma123@icloud.com>
2018-12-13 12:01:02 +0300

    lsn-01

--- app/index.php
+++ app/index.php
@@ -1,18 +1,16 @@
-<h1>hello world</h1>
-<h2>shwepsss</h2>
-<h3>OG</h3>
-<h4>jungle</h4>
-
 <?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 11.12.18
- * Time: 16:14
- */
-echo 'Hello world'.PHP_EOL;
-echo 'debugging ...';
-//xdebug.remote_host = 192.168.83.20
-//xdebug.remote_port = 9001
-//xdebug.remote_handler = dbgp
-//xdebug.idekey = PHPSTORM
+
+require 'application/lib/Dev.php';
+
+use application\core\Router;
+
+spl_autoload_register(function ($class) {
+    $path = str_replace('\\', '/', $class.'.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+session_start();
+
+$router = new Router;
