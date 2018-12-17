e52beaa5c43acbb3d5df160afc8c6df91b905662
Kudryashov <jahma123@icloud.com>
2018-12-17 12:16:09 +0300

    lsn-08 access control

---
+++ app/application/acl/main.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 17.12.18
+ * Time: 11:43
+ */
+return [
+    'all' => [
+    ],
+    'authorize' => [
+    ],
+    'guest' => [
+//
+    ],
+    'admin' => [
+//
+    ],
+];
